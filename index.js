7b5e0d74a86357a0f5fffb3aaeddfa17ae5aac77
black-creato <elkbryl@mail.ru>
2025-09-14 20:04:49 +0500

    slider with photo

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const btn = document.querySelector('.container__btn');
-const images = document.querySelector('.container__images');
-const loader = document.querySelector('#loader');
+const links = document.querySelectorAll('.container__link');
+const images = document.querySelectorAll('.slider__image');
+const dots = document.querySelectorAll('.slider__dots .dot');
+const arrowForward = document.querySelector('.next');
+const arrowBack = document.querySelector('.prev');
 
-btn.addEventListener('click',() => {
-  loader.style.display = "block";
+let currentIndex = 0;
 
-  fetch('https://dog.ceo/api/breeds/image/random/20')
-    .then((response) => response.json())
-    .then((data) => {
-      images.innerHTML = "";
-      data.message.forEach((img) => {
-        const imgElement = document.createElement('img');
-        imgElement.src = img;
-        imgElement.alt = 'Image`s Gallery'
-        images.appendChild(imgElement);
-      });
-    })
-    .catch((error) => {
-      console.log(error.message);
-    })
-    .finally(() => {
-      loader.style.display = "none";
-    }) 
-})
+function updateSlider(index) {
+  images.forEach(img => img.classList.remove('active'));
+  links.forEach(link => link.classList.remove('activies'));
+  dots.forEach(dot => dot.classList.remove('active'));
+
+  images[index].classList.add('active');
+  links[index].classList.add('activies');
+  dots[index].classList.add('active');
+}
+
+links.forEach((link, i) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    currentIndex = i;
+    updateSlider(currentIndex);
+  });
+});
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    currentIndex = i;
+    updateSlider(currentIndex);
+  });
+});
+
+arrowForward.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateSlider(currentIndex);
+});
+
+arrowBack.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateSlider(currentIndex);
+});
